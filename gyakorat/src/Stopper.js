3c836ff271f06947012067e91807a6b28cd71b88
Imre Varga <vargaimi990330@gmail.com>
2021-04-27 13:48:21 +0200

    stopper

--- gyakorat/src/Stopper.js
+++ gyakorat/src/Stopper.js
@@ -4,6 +4,7 @@ import Button from './Button';
 export default function Stopper() {
   const [timer, setTimer] = useState(0);
   const [running, setRunning] = useState(false);
+  const [kor, setKor] = useState([]);
 
   useEffect(() => {
     if (running) {
@@ -24,12 +25,27 @@ export default function Stopper() {
   function reset() {
     setTimer(0);
   }
+  function korido() {
+    const ujKor = { id: kor.length + 1, time: timer.toFixed(2) };
+    setKor([...kor, ujKor]);
+  }
+
   return (
     <div>
       <h1>{timer.toFixed(2)}</h1>
       <Button tipus="btn btn-danger m-2" fuggveny={stop} szoveg="Stop" />
       <Button tipus="btn btn-warning m-2" fuggveny={reset} szoveg="Reset" />
       <Button tipus="btn btn-success m-2" fuggveny={start} szoveg="Start" />
+      <Button tipus="btn btn-info m-2" fuggveny={korido} szoveg="Köridő" />
+      <div>
+        {kor.map(function (e) {
+          return (
+            <div>
+              {e.id} .kor {e.time}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
